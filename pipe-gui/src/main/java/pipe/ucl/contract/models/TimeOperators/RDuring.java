3c793a4a5e83d5e037b8647f004f6893c1fea80d
Gabriel <vanca.gabriel@gmail.com>
2018-02-18 17:26:09 +0000

    edited RD

--- pipe-gui/src/main/java/pipe/ucl/contract/models/TimeOperators/RDuring.java
+++ pipe-gui/src/main/java/pipe/ucl/contract/models/TimeOperators/RDuring.java
@@ -1,44 +1,23 @@
 package pipe.ucl.contract.models.TimeOperators;
 
-import pipe.ucl.constructor.controllers.Constructor;
 import pipe.ucl.contract.interfaces.GetDiscreteTime;
-import pipe.ucl.contract.models.ContractElement;
-import pipe.ucl.contract.models.EventElement;
-
-import java.util.ArrayList;
 
 public class RDuring extends TimeOperator {
 
     public final static String MainLabel = "RD";
     public final static String[] Labels = {"RD", "DURING"};
 
-    protected EventElement conditionalEvent;
-
-    public RDuring(GetDiscreteTime initialDate, GetDiscreteTime finalDate, EventElement conditionalEvent) {
+    public RDuring(GetDiscreteTime initialDate, GetDiscreteTime finalDate) {
         super(initialDate, finalDate);
-        this.conditionalEvent = conditionalEvent;
     }
 
     public RDuring(String[] parameters) {
         super(parameters);
-        if(parameters.length < 3) return;
-        ArrayList<ContractElement> contractElements = Constructor.MainContract.getContractElementsList();
-        for (ContractElement currentContractElement : contractElements) {
-            if (currentContractElement.getId().equals(parameters[2])) {
-                this.conditionalEvent = (EventElement) currentContractElement;
-                break;
-            }
-        }
     }
 
     @Override
     public String toString() {
-        if(conditionalEvent != null)
-            return MainLabel + "(" + initialDate + ", " + finalDate + " , " + conditionalEvent + ")";
         return MainLabel + "(" + initialDate + ", " + finalDate + ")";
     }
-
-    public EventElement getConditionalEvent() {
-        return conditionalEvent;
-    }
+    
 }
